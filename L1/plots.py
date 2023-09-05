395039985b34c68f4d5f70cf767eb9ef56ec88dc
Kireei <bjerned@gmail.com>
2023-09-05 14:02:40 +0200

    L1

--- L1/plots.py
+++ L1/plots.py
@@ -24,13 +24,15 @@ for filename in os.listdir(directory):
         #print(data) 
         size_vec = [float(line[4]) for line in data]
         mem_vec = [float(line[-2]) for line in data]
-        print(mem_vec)
+        mupds_vec = [float(line[-5]) for line in data]
+        #print(mem_vec)
         plt.xscale("log")
         
         plt.plot(size_vec, mem_vec)
         if count == 2:
             mem_512 = mem_vec.copy()
             size_512 = size_vec.copy()
+            mupds_float = mupds_vec.copy()
         if count == 4:
             plt.legend(["1", "256", "512", "768", "1024"])
             #plt.axes((512, 1e8, 0, 256))
@@ -41,9 +43,20 @@ for filename in os.listdir(directory):
             plt.clf()
             plt.xscale("log")
             plt.plot(size_512, mem_512)
+            
             plt.plot(size_vec, mem_vec)
+            
             plt.legend(["Float", "Double"])
             #plt.axes((512, 1e8, 0, 256))
+            plt.grid()
+            plt.show()
+            plt.clf()
+            plt.xscale("log")
+            print(mupds_float)
+            plt.plot(size_512, mupds_float)
+            
+            plt.plot(size_vec, mupds_vec)
+            plt.legend(["Float mupds", "Double mupds"])
             plt.grid()
             plt.show()
             
