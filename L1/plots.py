a245783fb151c6d5e31c22820ba8d4d454b3a0bc
Kireei <bjerned@gmail.com>
2023-09-07 11:26:01 +0200

    L1

--- L1/plots.py
+++ L1/plots.py
@@ -34,7 +34,10 @@ for filename in os.listdir(directory):
             size_512 = size_vec.copy()
             mupds_float = mupds_vec.copy()
         if count == 4:
+            plt.title("Block size")
             plt.legend(["1", "256", "512", "768", "1024"])
+            plt.ylabel("Memory throughput [GB/s]")
+            plt.xlabel("Vector size N [-]")
             #plt.axes((512, 1e8, 0, 256))
             plt.grid()
             plt.show()
@@ -42,10 +45,12 @@ for filename in os.listdir(directory):
         elif count == 5:
             plt.clf()
             plt.xscale("log")
+            plt.title("Memory bandwidth for different data types")
             plt.plot(size_512, mem_512)
             
             plt.plot(size_vec, mem_vec)
-            
+            plt.ylabel("Memory throughput [GB/s]")
+            plt.xlabel("Vector size N [-]")
             plt.legend(["Float", "Double"])
             #plt.axes((512, 1e8, 0, 256))
             plt.grid()
@@ -55,24 +60,33 @@ for filename in os.listdir(directory):
             print(mupds_float)
             plt.plot(size_512, mupds_float)
             
+            plt.title("Number of updates for different data types")
             plt.plot(size_vec, mupds_vec)
-            plt.legend(["Float mupds", "Double mupds"])
+            plt.legend(["Float", "Double"])
+            plt.ylabel("MUPD/s[s^-1]")
+            plt.xlabel("Vector size N [-]")
             plt.grid()
             plt.show()
             
         elif count == 7:
             mem_O3 = mem_vec.copy()
             size_O3 = size_vec.copy()
+            plt.title("Optimization flags")
             plt.xscale("log")
             plt.legend(["O2", "O3"])
+            plt.ylabel("Memory throughput [GB/s]")
+            plt.xlabel("Vector size N [-]")
             plt.grid()
             plt.show()
         elif count == 8:
             plt.clf()
+            plt.title("Local AMD R3 3600 against UPPMAX")
             plt.xscale("log")
             plt.plot(size_vec, mem_vec)
             plt.plot(size_O3, mem_O3)
             plt.legend(["Local", "UPPMAX"])
+            plt.ylabel("Memory throughput [GB/s]")
+            plt.xlabel("Vector size N [-]")
             #plt.axes((512, 1e8, 0, 256))
             plt.grid()
             plt.show()
@@ -80,10 +94,26 @@ for filename in os.listdir(directory):
         elif count == 10:
             
             plt.xscale("log")
+            plt.title("Memory alignment -O3, UPPMAX")
             plt.legend(["Non-aligned", "Aligned"])
+            plt.ylabel("Memory throughput [GB/s]")
+            plt.xlabel("Vector size N [-]")
             #plt.axes((512, 1e8, 0, 256))
             plt.grid()
             plt.show()
+            plt.clf()
+        elif count == 12:
+            plt.xscale("log")
+            plt.title("Memory alignment -O2, AMD R3 3600")
+            plt.legend(["Non-aligned", "Aligned"])
+            plt.ylabel("Memory throughput [GB/s]")
+            plt.xlabel("Vector size N [-]")
+            #plt.axes((512, 1e8, 0, 256))
+            plt.grid()
+            plt.show()
+            
+            
+        
             
             
             
