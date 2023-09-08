fe0a88b8faab528499844fbe76106b6085cd2785
Erik Bjerned <ebjerned@rackham3.uppmax.uu.se>
2023-09-08 13:40:02 +0200

    L1

--- L1/single.sh
+++ L1/single.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2023-2-36 # project name 
-#SBATCH -M snowy # name of system 
-#SBATCH -p node # request a full node 
-#SBATCH -N 1 # request 1 node 
-#SBATCH -t 15:00 # job takes at most 1 hour 
-#SBATCH -J stream_simd # name of the job 
-#SBATCH -D ./ # stay in current working directory 
-
-
-./stream_triadSIMDO2 -min 512 -max 1e8 -align 1 
+#SBATCH -A uppmax2023-2-36 # project name
+#SBATCH -M snowy # name of system
+#SBATCH -p node # request a full node
+#SBATCH -N 1 # request 1 node
+#SBATCH -t 15:00 # job takes at most 1 hour
+#SBATCH --gres=gpu:1 --gpus-per-node=1
+#SBATCH -J stream_simd # name of the job
+#SBATCH -D ./ # stay in current working directory
+./stream_triad_cuda -min 512 -max 1e8
