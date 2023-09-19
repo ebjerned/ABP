903a37212e1e77cc4c4a4b7c6ac7a252952bbce7
Erik Bjerned <ebjerned@s174.uppmax.uu.se>
2023-09-19 20:59:31 +0200

    L2

--- L2/matmul.cu
+++ L2/matmul.cu
@@ -5,6 +5,17 @@
 #include <iostream>
 #include <vector>
 
+#define AssertCuda(error_code) \
+if(error_code != cudaSuccess) 	\
+{ \
+	std::cout << "The cuda call in" << __FILE__ << " on line "\
+	<< __LINE__ << " resulted in the error '" \
+	<< cudaGetErrorString(error_code) << "'" << std::endl;\
+	std::abort();\
+}\
+
+
+
 
 const int block_size = 128;
 const int chunk_size = 1;
@@ -32,14 +43,14 @@ __global__ void set_vector(const int N, const float val, float *x)
     {
       const int idx = idx_base + i * block_size;
       if (idx < N)
-        x[idx] = val;
+        x[idx] = val*(blockIdx.x*blockDim.x+threadIdx.x + threadIdx.y*gridDim.x*blockDim.x);
     }
 }
 
 
 __global__ void matmul(const float*A, const float*B, float* C, unsigned const int M, unsigned const int N){
-	size_t i{blockIdx.y*blockDim.y + threadIdx.y};
-	size_t j{blockIdx.x*blockDim.x + threadIdx.x};
+	size_t i = blockIdx.y*blockDim.y + threadIdx.y;
+	size_t j = blockIdx.x*blockDim.x + threadIdx.x;
 
 	if((i>= M) || (j >= N)){
 		return;
@@ -48,12 +59,13 @@ __global__ void matmul(const float*A, const float*B, float* C, unsigned const in
 	float acc_sum = 0;
 	for(unsigned int k = 0; k<N; ++k)
 	{
-		acc_sum += A[i*N+k]*B[k*N+j];
+		acc_sum += A[i+k*N]*B[k+j*N];
 	}
+//	__syncthreads();
+	
 	C[i*N+j] = acc_sum;
 
 }
-
 
 
 // Run the actual benchmark
@@ -61,23 +73,38 @@ void benchmark_triad(const bool        align,
                      const std::size_t N,
                      const long long   repeat)
 {
+
+  unsigned int elementsSidePerBlock = 1;
+  dim3 blockDimensions(ceil(N/elementsSidePerBlock),ceil(N/elementsSidePerBlock));
+  cudaError_t errorCode;
+
   float *v1, *v2, *v3;
   // allocate memory on the device
-  cudaMalloc(&v1, N * N * sizeof(float));
-  cudaMalloc(&v2, N * N * sizeof(float));
-  cudaMalloc(&v3, N * N * sizeof(float));
+  errorCode =  cudaMalloc(&v1, N * N * sizeof(float));
+  AssertCuda(errorCode);
+  errorCode = cudaMalloc(&v2, N * N * sizeof(float));
+  AssertCuda(errorCode);
+  errorCode = cudaMalloc(&v3, N * N * sizeof(float));
+  AssertCuda(errorCode);
 
   const unsigned int n_blocks = (N + block_size - 1) / block_size;
 
-  set_vector<<<n_blocks, block_size>>>(N, 1.f, v1);
-  set_vector<<<n_blocks, block_size>>>(N, 1.f, v2);
-  set_vector<<<n_blocks, block_size>>>(N, 0.f, v3);
+  set_vector<<<blockDimensions, block_size>>>(N*N, 1.f, v1);
+  errorCode = cudaGetLastError();
+  AssertCuda(errorCode);
+  set_vector<<<blockDimensions, block_size>>>(N*N, 1.f, v2);
+  errorCode = cudaGetLastError();
+  AssertCuda(errorCode);
+  set_vector<<<blockDimensions, block_size>>>(N*N, 0.f, v3);
+  errorCode = cudaGetLastError();
+  AssertCuda(errorCode);
 
   std::vector<float> result_host(N*N);
 
   const unsigned int           n_tests = 20;
-  const unsigned long long int n_repeat =
-    repeat > 0 ? repeat : std::max(1UL, 100000000U / N);
+  /*const unsigned long long int n_repeat =
+    repeat > 0 ? repeat : std::max(1UL, 100000000U / N);*/
+  const unsigned int n_repeat = 1;
   double best = 1e10, worst = 0, avg = 0;
   for (unsigned int t = 0; t < n_tests; ++t)
     {
@@ -85,8 +112,9 @@ void benchmark_triad(const bool        align,
       const auto t1 = std::chrono::steady_clock::now();
 
       for (unsigned int rep = 0; rep < n_repeat; ++rep)
-      matmul<<<n_blocks, block_size>>>(v1, v2, v3, N, N);
-
+      matmul<<<blockDimensions, block_size>>>(v1, v2, v3, N, N);
+	  errorCode = cudaGetLastError();
+  	  AssertCuda(errorCode);
       cudaDeviceSynchronize();
       // measure the time by taking the difference between the time point
       // before starting and now
@@ -101,24 +129,34 @@ void benchmark_triad(const bool        align,
     }
 
   // Copy the result back to the host
-  cudaMemcpy(result_host.data(), v3, N * N * sizeof(float), cudaMemcpyDeviceToHost);
-  for(unsigned int i = 0; i <N*N;++i)
-  	std::cout << result_host[i];
-  if ((result_host[0] + result_host[N - 1]) != 526.f)
+  errorCode = cudaMemcpy(result_host.data(), v3, N * N * sizeof(float), cudaMemcpyDeviceToHost);  
+  AssertCuda(errorCode);
+
+
+/* for(unsigned int i = 0; i <N*N;++i){
+  	std::cout << result_host[(i*N)%(N*N)+(i/N)] << " ";
+	if (i % N == N-1) std::cout << "" << std::endl;
+  }*/
+
+  //Not perfect check for correctness, works for 8 but not for 512 or larger
+  if (result_host[0] != N*((N-1)*N*(2*N-1)/6))
     std::cout << "Error in computation, got "
               << (result_host[0] + result_host[N - 1]) << " instead of 526"
               << std::endl;
 
   // Free the memory on the device
-  cudaFree(v1);
-  cudaFree(v2);
-  cudaFree(v3);
+  errorCode = cudaFree(v1);
+  AssertCuda(errorCode);
+  errorCode = cudaFree(v2);
+  AssertCuda(errorCode);
+  errorCode = cudaFree(v3);
+  AssertCuda(errorCode);
 
   std::cout << "STREAM triad of size " << std::setw(8) << N
             << " : min/avg/max: " << std::setw(11) << best << " "
             << std::setw(11) << avg / n_tests << " " << std::setw(11) << worst
-            << " seconds or " << std::setw(8) << 1e-6 * N / best
-            << " MUPD/s or " << std::setw(8)
+            << " seconds or " << std::setw(8) << 1e-9 * 2 * N * N * N / best
+            << " GFLOPS/s or " << std::setw(8)
             << 1e-9 * 3 * sizeof(float) * N / best << " GB/s" << std::endl;
 }
 
