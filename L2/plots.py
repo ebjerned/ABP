198db8ac31ed05a2131fe14d4fa669c59aee923e
Kireei <bjerned@gmail.com>
2023-09-22 11:04:49 +0200

    L2

--- L2/plots.py
+++ L2/plots.py
@@ -26,13 +26,15 @@ for filename in os.listdir(directory):
         sizeM_vec = [float(line[5]) for line in data]
         mem_vec = [float(line[-2]) for line in data]
         gflops_vec = [float(line[-5]) for line in data]
-        styles = ["-", "-", "-", "-", "-", "-", "k--", "-", "k--", "-", "k--"]
+        styles = ["-", "-", "-", "-", "-", "-", "k--", "-", "k--", "-", "k--", "-", "k--"]
         #print(mem_vec)
         plt.xscale("log")
         
         
         if(count == 9 or count == 10):
             plt.plot(sizeM_vec, mem_vec, styles[count], linewidth=1)
+        elif(count == 11 or count == 12):
+            plt.plot(sizeN_vec, gflops_vec, styles[count], linewidth=1)
         else:
             plt.plot(sizeN_vec, mem_vec, styles[count], linewidth=1)
         
@@ -62,7 +64,17 @@ for filename in os.listdir(directory):
             plt.title("Matrix-vector multiplication, fixed M=16384")
             plt.ylabel("Memory throughput [GB/s]")
             plt.xlabel("Size N [-]")
-            plt.savefig("MatvecfN.png")
+            plt.savefig("MatvecfM.png")
+            plt.show()
+            plt.clf()
+            
+        if count == 12:
+            plt.legend(["Implemented", "cuBLAS"])
+            plt.grid()
+            plt.title("Matrix-matrix multiplication, M=N=K")
+            plt.ylabel("Memory throughput [GB/s]")
+            plt.xlabel("Size N [-]")
+            plt.savefig("Matmat.png")
             plt.show()
             plt.clf()
             
