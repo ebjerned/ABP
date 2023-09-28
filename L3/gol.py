090fbcda878803cbe707d0c4907430956c24779b
Kireei <bjerned@gmail.com>
2023-09-28 09:45:50 +0200

    L3

--- L3/gol.py
+++ L3/gol.py
@@ -11,22 +11,6 @@ if platform.system() == 'Windows':
     board = lifereader.readlife('C:/Users/Erik Bjerned/Documents/Git_repos/ABP/L3/files/BREEDER3.LIF', 2048)
 else:
     board = lifereader.readlife('files/BREEDER3.LIF', 2048)
-    
-
-plt.figure(figsize=(20,20))
-plotstart=924
-plotend=1124
-plt.imshow(board[plotstart:plotend,plotstart:plotend])
-
-plt.figure(figsize=(20,20))
-plt.imshow(board)
-#plt.show()
-
-#tf.config.set_visible_devices([], 'GPU')
-#tf.debugging.set_log_device_placement(True)
-#
-#boardtf = tf.cast(board, dtype=tf.float16)
-#
 
 def perform_tests(board, benchCpu=False, dtypes=[tf.float16]):
     times = []
@@ -52,8 +36,6 @@ def perform_tests(board, benchCpu=False, dtypes=[tf.float16]):
     print(times)
     return times
         
-    
-    
 def timed_life(board):
     tic = timeit.default_timer()
     boardresult = runlife(board, 1000);
@@ -66,40 +48,22 @@ def timed_life(board):
 
 @tf.function
 def runlife(board, iters):
-    # Init work
     board = tf.reshape(board, [1, 2048, 2048, 1])
     filter_kernel= tf.cast(np.array([1,1,1,1,0,1,1,1,1]), dtype=board.dtype)
     filter_kernel = tf.reshape(filter_kernel, [3,3, 1,1])
     for i in range(iters):
-        # Perform convolution
         count = tf.nn.conv2d(board,filter_kernel, 1, "SAME")
         # In the case of having 2 neighbours, the cell has to be alive. 
         # All counts of 3 will be alive in next iteration.
         survive = tf.logical_and(tf.equal(board, 1), tf.equal(count,2))
         born = tf.equal(count, 3)
-        
-        # Then, update the board by keeping these tensors
         board = tf.cast(tf.logical_or(survive, born),board.dtype)
         
-        #print(i)
-        
-    # Final work
     board = tf.reshape(board, [2048,2048])
     return board
 
 
 times = perform_tests(board,benchCpu=False, dtypes=[tf.float16, tf.float32, tf.bfloat16])
 
+## FOR TESTING CPU PERFOMANCE
 #times = perform_tests(board,benchCpu=True, dtypes=[tf.float16, tf.float32, tf.bfloat16])
-
-#tic = timeit.default_timer()
-#boardresult = runlife(boardtf, 1000);
-#toc = timeit.default_timer();
-#print("Compute time: " + str(toc - tic))
-#result = np.cast[np.int32](boardresult);
-#print("Cells alive at start: " + str(np.count_nonzero(board)))
-#print("Cells alive at end:   " + str(np.count_nonzero(result)))
-#print(np.count_nonzero(result))
-#plt.figure(figsize=(20,20))
-#plt.imshow(result[plotstart:plotend,plotstart:plotend])
-#plt.show()
